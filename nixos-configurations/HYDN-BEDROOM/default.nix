d8f93ccaa53d9eebcda856bbed8770f6b2361897
Martí Llinàs <marti.llinas.comas@gmail.com>
2022-06-18 11:57:13 +0200

    Simplify nixos configs based on WSL

--- nixos-configurations/HYDN-BEDROOM/default.nix
+++ nixos-configurations/HYDN-BEDROOM/default.nix
@@ -1,10 +1,14 @@
 { self, system, nixpkgs, home-manager, nixos-wsl }:
 
-nixpkgs.lib.nixosSystem {
-  inherit system;
-  specialArgs = { inherit  nixpkgs home-manager nixos-wsl; };
-  modules = [
-    self.nixosModules.wsl
-    ./configuration.nix
-  ];
-}
+let
+  hostName = "HYDN-BEDROOM";
+  hostNameModule = { ... }: { networking.hostName = hostName; };
+in
+  nixpkgs.lib.nixosSystem {
+    inherit system;
+    specialArgs = { inherit nixpkgs home-manager nixos-wsl; };
+    modules = [
+      self.nixosModules.wsl
+      hostNameModule
+    ];
+  }
