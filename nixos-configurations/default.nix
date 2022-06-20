ce78bcd2c33c11ac09757bb3de2058d4de34ba72
Martí Llinàs <marti.llinas.comas@gmail.com>
2022-06-20 09:22:37 +0200

    Remove system as parameter for wsl configs

--- nixos-configurations/default.nix
+++ nixos-configurations/default.nix
@@ -1,7 +1,6 @@
 { self, system, nixpkgs, home-manager, nixos-wsl }:
 
 {
-  #HYDN-BEDROOM = import ./HYDN-BEDROOM { inherit self system nixpkgs home-manager nixos-wsl; };
 }
 //
-import ./wsl-configs { inherit self system nixpkgs home-manager nixos-wsl; }
+import ./wsl-configs { inherit self nixpkgs home-manager nixos-wsl; }
--- nixos-configurations/wsl-configs/default.nix
+++ nixos-configurations/wsl-configs/default.nix
@@ -1,16 +1,20 @@
-{ self, system, nixpkgs, home-manager, nixos-wsl }:
+{ self, nixpkgs, home-manager, nixos-wsl }:
 
 let
+  system = "x86_64-linux";
+
   allHostNames = f: hnList: builtins.listToAttrs (builtins.map (e: { name = e; value = f e;}) hnList);
   hostNames = import ./hostNames.nix;
-  hostNameToHostNameModule = hn: { ... }: { networking.hostName = hn; };
+
+  mkHostNameModule = hn: { ... }: { networking.hostName = hn; };
+
   hostNameToNixosConfig = hn:
     nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit nixpkgs home-manager nixos-wsl; };
       modules = [
         self.nixosModules.wsl
-        (hostNameToHostNameModule hn)
+        (mkHostNameModule hn)
       ];
     };
 in
